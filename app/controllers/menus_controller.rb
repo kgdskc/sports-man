cbc85edac4d085aade4c4b53d8f1a26b53b82d6b
kgdskc <wajima0503@gmail.com>
2021-08-13 12:48:59 +0000

    [Add]

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -2,22 +2,41 @@ class MenusController < ApplicationController
   def index
     @menus = Menu.all
   end
-  
+
   def new
+    @menu = Menu.new
   end
-  
+
   def show
+    @menu = Menu.find(params[:id])
   end
-  
+
   def edit
+    @menu = Menu.find(params[:id])
   end
-  
+
   def create
+    @menu = Menu.new(menu_params)
+    @menu.user_id = current_user.id
+    @menu.save
+    redirect_to menus_path
   end
-  
+
   def update
+    @menu = Menu.find(params[:id])
+    @menu.update(menu_params)
+    redirect_to menus_path
   end
-  
+
   def destroy
+    @menu = Menu.find(params[:id])
+    @menu.destroy
+    redirect_to menus_path
+  end
+
+  private
+
+  def menu_params
+    params.require(:menu).permit(:user_id, :image, :body, :estimation_ingestion_cal, :menu_name)
   end
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '577a4207a03be1729e061440d81a7e506120b63dc91a6a08519caff38a1066076b02cb3e3096b7a1bb38e23c75148bc5646ec25b1f6e2b9768dba9e836dc5e96'
